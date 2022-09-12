93d2672c37321e4b6afcb13953289d5fc0cf2602
Anna Clausen <anna.revall.clausen@gmail.com>
2022-09-11 21:35:45 -0700

    commit tree 833738ebe05f6cf1e83aca4a052af76a3c55a558

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+    name_array.map {|name| "Hello, my name is #{name}."}
+end
+
+#takes an array as an argument
+def assign_rooms(name_array)
+    room_assignments = name_array.map 
+    room_assignments.each.with_index(1) do |name, index| 
+        "Hello, #{name}! You'll be assigned to room #{index}!" 
+    end
+end
